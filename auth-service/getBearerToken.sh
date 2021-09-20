c256b1a29800fa3fc6ddfb2e57f2524dd099e1c1
Shree Sagar <39877755+shreesagar3@users.noreply.github.com>
2021-09-20 23:57:11 +0530

    Update getBearerToken.sh

--- auth-service/getBearerToken.sh
+++ auth-service/getBearerToken.sh
@@ -1,7 +1,7 @@
 curl --location --request POST 'https://onemoon.us.auth0.com/oauth/token' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---data-urlencode 'client_id=CCiNriV5CkVfsHqJ6phjkHCBi9SmonmK' \
+--data-urlencode 'client_id=' \
 --data-urlencode 'username=admin@charityauction.com' \
---data-urlencode 'password=Shree@0786' \
+--data-urlencode 'password=' \
 --data-urlencode 'grant_type=password' \
 --data-urlencode 'scope=openid'
